e1c1f969ac7fb03fdfceaf6716ca94848c4faf11
Maximus <tomiwadairo@gmail.com>
2024-06-19 00:03:19 +0100

    fixed more apos

--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -15,16 +15,16 @@ const Hero = () => {
     <section className=" relative xl:padding-l xl:padding-t">
       <section id="home" className='min-h-screen max-container justify-center gap-10 flex flex-col xl:flex-row  w-full'>
         <div className=' relative z-20 xl:w-2/5 flex flex-col justify-center items-start w-full pt-28 max-xl:padding-x'>
-          <p className='text-xl font-montserrat text-coral-red'>Awon Bata fun asiko 'Harmattan' ti a wa yii</p>
+          <p className='text-xl font-montserrat text-coral-red'>Awon Bata fun asiko &apos;Harmattan&apos; ti a wa yii</p>
           <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-semibold'>
             <span className='  xl:bg-white  rounded-full xl:whitespace-nowrap  pr-10'>
               Tuntun ti Wolu o 
             </span>
             <br /> 
-            <span className='text-coral-red inline-block font-bold mt-3'>Bata'&apos;'</span> Shoes
+            <span className='text-coral-red inline-block font-bold mt-3'>Bata&apos;</span> Shoes
             
           </h1>
-          <p className='  font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>E fo'ju yin lounje. O tuntun, O dun' wo, O tun wa dani lara ya fun igbadun aye kan yii</p>
+          <p className='  font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>E fo&apos;ju yin lounje. O tuntun, O dun&apos; wo, O tun wa dani lara ya fun igbadun aye kan yii</p>
           <Button label='Ra meta sile' iconUrl={arrowRight} />
           <div className=" gap-16 flex mt-20 justify-start items-start flex-wrap w-full ">
             {statistics.map((eachStat,index) => (
